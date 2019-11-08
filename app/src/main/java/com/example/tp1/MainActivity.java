fa350fb4359b16cda8fa133f207ee8b3bed279d6
Khalil Hammami <khammami@gmail.com>
2019-11-08 15:41:11 +0100

    fixes

--- app/src/main/java/com/example/tp1/MainActivity.java
+++ app/src/main/java/com/example/tp1/MainActivity.java
@@ -6,19 +6,19 @@ import android.util.Log;
 
 
 public class MainActivity extends AppCompatActivity {
-    private static final String LOG_TAG = "MyActivity";
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("LOG_TAG", "Debug ");
-        Log.e("LOG_TAG", " Error");
-        Log.w("LOG_TAG", "Warn");
-        Log.i("LOG_TAG", " Info");
-        Log.v("LOG_TAG", " verbose");
-        Log.i("LOG_TAG", "  le nom du fichier de mise en page de l'activité principale: MainActivity.java ");
-        Log.i("LOG_TAG", "  le nom de la ressource chaîne qui spécifie le nom de l'application: app_name ");
-        Log.i("LOG_TAG", " pour créer un nouvel émulateur on utilise AVD Manager");
-        Log.i("LOG_TAG", " le volet Logcat si le menu de niveau Log est réglé sur lequel l'élément Info");
+        Log.d(LOG_TAG, "Debug ");
+        Log.e(LOG_TAG, " Error");
+        Log.w(LOG_TAG, "Warn");
+        Log.i(LOG_TAG, " Info");
+        Log.v(LOG_TAG, " verbose");
+        Log.i(LOG_TAG, "  le nom du fichier de mise en page de l'activité principale: MainActivity.java ");
+        Log.i(LOG_TAG, "  le nom de la ressource chaîne qui spécifie le nom de l'application: app_name ");
+        Log.i(LOG_TAG, " pour créer un nouvel émulateur on utilise AVD Manager");
+        Log.i(LOG_TAG, " le volet Logcat si le menu de niveau Log est réglé sur lequel l'élément Info");
     }
 }
